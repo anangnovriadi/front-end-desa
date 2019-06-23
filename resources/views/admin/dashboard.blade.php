20643c2b30680818d1aed4a193f882e3f80e4d85
anang <anangnov99@gmail.com>
2019-06-23 21:32:44 +0700

    admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,7 +17,7 @@
                             {{-- <h4>Admin Dashboard CMS Desa Digital</h4> --}}
                         </div>
                         <div class="card-body">
-                            <h4><h4>Admin Dashboard CMS Desa Digital</h4></h4>
+                            <u><h4>Admin Dashboard CMS Desa Digital</h4></h4>
                         </div>
                     </div>
                     </div>
